b598a67052736653cd4ababfb73553546fd316e0
shopa.tomek@gmail.com <shopa.tomek@gmail.com>
2023-09-08 11:57:07 +0200

    Packages '@auth/prisma-adapter' and 'bcrypt' have been installed

---
+++ app/layout.tsx
@@ -0,0 +1,35 @@
+import ClientOnly from "./components/ClientOnly";
+// import Modal from "./components/modals/Modal";
+import RegisterModal from "./components/modals/RegisterModal";
+import Navbar from "./components/navbar/Navbar";
+import "./globals.css";
+import { Nunito } from "next/font/google";
+import ToasterProvider from "./providers/ToasterProvider";
+
+export const metadata = {
+  title: "Zoomeed",
+  description: "Zoomeed educational",
+};
+
+const font = Nunito({
+  subsets: ["latin-ext"],
+});
+
+export default function RootLayout({
+  children,
+}: {
+  children: React.ReactNode;
+}) {
+  return (
+    <html lang="en">
+      <body className={font.className}>
+        <ClientOnly>
+          <ToasterProvider />
+          <RegisterModal />
+          <Navbar />
+        </ClientOnly>
+        {children}
+      </body>
+    </html>
+  );
+}
