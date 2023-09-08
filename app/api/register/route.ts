667ddc779c2edb301c499cba73a596df86361207
shopa.tomek@gmail.com <shopa.tomek@gmail.com>
2023-09-08 16:45:35 +0200

    first 4 tests of registration autorization ended successfully with code 200. 4 users created with proper name, email, date and hashedPasswords. Comment added as well to route.ts file

--- app/api/register/route.ts
+++ app/api/register/route.ts
@@ -17,3 +17,11 @@ export async function POST(request: Request) {
 
   return NextResponse.json(user)
 }
+// Ten kod definiuje funkcję POST, która obsługuje żądanie HTTP typu POST. Głównymi krokami w tej funkcji są:
+
+// Parsowanie treści żądania jako JSON i przypisanie jej do zmiennej body.
+// Wyodrębnienie wartości email, name i password z obiektu body.
+// Haszowanie hasła za pomocą biblioteki bcrypt przy użyciu funkcji bcrypt.hash. Hasło zostaje przekazane jako pierwszy argument, a liczba rund hashowania (12) jako drugi argument.
+// Tworzenie nowego użytkownika w bazie danych przy użyciu obiektu prisma.user.create. Podane wartości email, name i hashedPassword są przekazane jako część danych dla nowego użytkownika.
+// Zwracanie odpowiedzi HTTP w formacie JSON, zawierającej utworzonego użytkownika.
+// Podsumowując, ten kod obsługuje żądanie POST, które zawiera dane użytkownika (email, name, password). Hasło jest haszowane przy użyciu biblioteki bcrypt, a następnie tworzony jest nowy użytkownik w bazie danych za pomocą biblioteki prisma. Na koniec, zwracana jest odpowiedź HTTP zawierająca utworzonego użytkownika w formacie JSON.
