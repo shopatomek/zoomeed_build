4d9b3672ce1294be912ab55d0aef6d65b88e5b04
shopa.tomek@gmail.com <shopa.tomek@gmail.com>
2023-09-10 22:37:48 +0200

    currentUser objects has been added to Navbar. Line 33 in the layout.tsx file. currentUser has been proped in the Navbar component. Now logged in user could be seen in Dev Tools

--- app/layout.tsx
+++ app/layout.tsx
@@ -6,6 +6,7 @@ import Navbar from "./components/navbar/Navbar";
 import "./globals.css";
 import { Nunito } from "next/font/google";
 import ToasterProvider from "./providers/ToasterProvider";
+import getCurrentUser from "./actions/getCurrentUser";
 
 export const metadata = {
   title: "Zoomeed",
@@ -16,19 +17,20 @@ const font = Nunito({
   subsets: ["latin-ext"],
 });
 
-export default function RootLayout({
+export default async function RootLayout({
   children,
 }: {
   children: React.ReactNode;
 }) {
+  const currentUser = await getCurrentUser();
   return (
     <html lang="en">
       <body className={font.className}>
         <ClientOnly>
           <ToasterProvider />
-          <LoginModal/>
+          <LoginModal />
           <RegisterModal />
-          <Navbar />
+          <Navbar currentUser={currentUser} />
         </ClientOnly>
         {children}
       </body>
